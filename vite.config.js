24176b8994908f5550c7a0410084f2330a98d86a
Eugene Buyakin <eugene.buyakin@gmail.com>
2025-10-11 17:26:58 +0100

    feat:landing page done, app is moved to app.html. all html are moved to root.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // Use a subpath when deploying to GitHub Pages: https://<user>.github.io/<repo>/
 const repoName = 'maichat'
 const isCI = process.env.GITHUB_ACTIONS === 'true'
-const hasCustomDomain = true // Set this to true since we're using maichat.io
+const hasCustomDomain = true // Set this to true since we're using maichat.app
 
 export default defineConfig({
-  base: isCI ? `/${repoName}/` : '/',
   base: isCI && !hasCustomDomain ? `/${repoName}/` : '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app.html'),
+        tutorial: resolve(__dirname, 'tutorial.html'),
+      }
+    }
+  }
 })
